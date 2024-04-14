99041aa8873c8a6798633133ac5dacbacd84e536
Ogalo <ogaloharrison@gmail.com>
2024-04-14 11:16:30 -0400

    Add session routers

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,9 @@
-import NextAuth from "next-auth/next"
-import GoogleProvider from 'next-auth/providers/google'
-import { signIn } from "next-auth/react"
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
 
+import User from "@models/user";
 
+import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
   providers: [
@@ -11,18 +12,38 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })
   ],
-  async session({session}){
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
 
+      session.user.id = sessionUser._id.toString();
+
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        // serverless -> lambda ->  dynamodb
+        await connectToDB();
+
+        // check if a user already exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        //if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
-  async signIn({ profile }){
-    try {
-     // serverless -> lambda ->  dynamodb
+});
 
-    } catch (error){
-
-    }
-
-  }
-})
-
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
