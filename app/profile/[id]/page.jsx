0d54f313619d60998423a043676e08cfd91779a6
Ogalo <ogaloharrison@gmail.com>
2024-04-24 03:49:46 -0400

    Add Suspense on useSearchParams

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,6 +1,7 @@
 'use client'
 import React, {useEffect, useState } from 'react'
 import {useSearchParams } from 'next/navigation'
+import { Suspense }  from 'react'
 
 import Profile from '@components/Profile'
 
@@ -22,11 +23,14 @@ const UserProfile = ({params}) => {
   }, [params.id]);
 
   return (
-    <Profile
-      name={userName}
-      desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
-      data={userPosts}
-    />
+    <Suspense>
+
+      <Profile
+        name={userName}
+        desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
+        data={userPosts}
+      />
+    </Suspense>
   )
 }
 
--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
@@ -57,13 +57,16 @@ const EditPrompt = () => {
   };
 
   return (
-    <Form
-      type='Edit'
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense>
+      <Form
+        type='Edit'
+        prompt={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+
+    </Suspense>
   );
 };
 
